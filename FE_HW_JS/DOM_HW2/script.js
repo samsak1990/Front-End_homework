04850e8fbc136e4ebd797253f226d577807f0ecd
PashaK <samsak.developer@internet.ru>
2024-06-21 14:44:21 +0300

    Five task done

--- FE_HW_JS/DOM_HW2/script.js
+++ FE_HW_JS/DOM_HW2/script.js
@@ -1,13 +1,108 @@
+const bodyTag = document.body;
+
 // Создайте кнопку и абзац в html. При нажатии на кнопку изменяйте содержимое абзаца
+const div1 = document.createElement("div");
+const newBtn1 = document.createElement("button");
+const newParag1 = document.createElement("p");
+
+newBtn1.innerText = "Изменить текст";
+newParag1.innerText = "Старый контект этого параграфа!";
+
+newBtn1.addEventListener("click", (e) => {
+  e.preventDefault();
+  newParag1.innerText = "А вот и новый текст!";
+});
+
+bodyTag.append(div1);
+div1.append(newParag1, newBtn1);
 
 // Создайте кнопку и список в html. При нажатии на кнопку добавляйте новый элемент в конец списка
 
+const div2 = document.createElement("div");
+const newBtn2 = document.createElement("button");
+const newUl = document.createElement("ul");
+let count = 0;
+
+newBtn2.innerText = "Добавить элемент списка";
+
+newBtn2.addEventListener("click", (e) => {
+  e.preventDefault();
+  count += 1;
+  const newLi = document.createElement("li");
+  newLi.innerText = `Элемент # ${count}`;
+  newUl.appendChild(newLi);
+});
+
+bodyTag.append(div2);
+div2.append(newBtn2, newUl);
+
 // Создайте кнопку и абзац в html. При нажатии на кнопку добавляйте новый абзац перед существующим
+const div3 = document.createElement("div");
+const newBtn3 = document.createElement("button");
+let flag = 0;
+
+div3.classList.add("threeDiv");
+newBtn3.innerText = "Добавить абзац";
+
+bodyTag.append(div3);
+div3.append(newBtn3);
+
+newBtn3.addEventListener("click", (e) => {
+  e.preventDefault();
+  flag += 1;
+  const addP = document.createElement("p");
+  addP.innerText = `Абзац # ${flag}`;
+  div3.insertAdjacentElement("afterbegin", addP);
+});
 
 // Создайте кнопку и абзац в html. При нажатии на кнопку изменяйте стиль абзаца (например, цвет текста и фон)
 
+const div4 = document.createElement("div");
+const newBtn4 = document.createElement("button");
+const newParag4 = document.createElement("p");
+
+newParag4.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere tempore corrupti debitis sit hic, quis sed, expedita nostrum ea, reprehenderit numquam. A quia asperiores iusto sint exercitationem quidem cum numquam!`;
+
+newBtn4.innerText = "Изменить стиль абзаца";
+
+newBtn4.addEventListener("click", (e) => {
+  e.preventDefault();
+  newParag4.style.cssText = `color: blue; 
+    font-weight: bold; 
+    text-decoration: underline;`;
+});
+
+bodyTag.append(div4);
+div4.append(newParag4, newBtn4);
+
 // Создайте кнопку и абзац в html. При нажатии на кнопку применяйте к абзацу несколько стилей
+
+const div5 = document.createElement("div");
+const newBtn5 = document.createElement("button");
+const newParag5 = document.createElement("p");
+
+newBtn5.innerText = "Добавить стили";
+
+newParag5.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere tempore corrupti debitis sit hic, quis sed, expedita nostrum ea, reprehenderit numquam. A quia asperiores iusto sint exercitationem quidem cum numquam!`;
+
+newBtn5.addEventListener("click", (e) => {
+  e.preventDefault();
+  newParag5.classList.add("boldColorSize");
+});
+
+bodyTag.append(div5);
+div5.append(newParag5, newBtn5);
 
 // Создайте кнопку и абзац в html. При нажатии на кнопку вставляйте HTML-код рядом с абзацем с использованием insertAdjacentHTML
 
 // Создайте кнопку и абзац в html. Для абзац задайте стили в css файле. При нажатии на кнопку выводите в консоль значение стиля (например, цвет текста) абзаца с использованием getComputedStyl
+
+/*-----------------------------------------*/
+const divAll = document.querySelectorAll("div");
+
+divAll.forEach((element) => {
+  element.style.border = "1px";
+  element.style.borderColor = "black";
+  element.style.borderStyle = "solid";
+  element.style.padding = "10px";
+});
