11573a5de9af2d19dfd322fd97bf7096a16892d2
samsak1990 <159149362+samsak1990@users.noreply.github.com>
2024-08-05 12:30:28 +0300

    Add files via upload

--- FE_HW_JS/List_of_employee/script.js
+++ FE_HW_JS/List_of_employee/script.js
@@ -13,56 +13,60 @@ document.addEventListener("DOMContentLoaded", () => {
         </tr>`;
   }
 
-  function whereClick(event) {
-    const elementClick = event.target.localName;
-    switch (elementClick) {
-      case "button":
-        addNewRow();
-        break;
-      case "td":
-        chooseCellForChanges(event);
-        break;
-      default:
-        // console.log("Больше нового ничего не придумали:)");
-        break;
-    }
-  }
+  // function whereClick(event) {
+  //   const elementClick = event.target.localName;
+  //   switch (elementClick) {
+  //     case "button":
+  //       addNewRow();
+  //       break;
+  //     case "td":
+  //       chooseCellForChanges(event);
+  //       break;
+  //     default:
+  //       // console.log("Больше нового ничего не придумали:)");
+  //       break;
+  //   }
+  // }
 
   function chooseCellForChanges(event) {
-    const clickedTd = event.target;
-    const firstValue = clickedTd.textContent;
-    const typeInput = clickedTd.dataset.type;
-    clickedTd.classList.add("editing");
-    clickedTd.innerHTML = `<input type=${typeInput} min="18" max="65">`;
+    const initialValue = event.target;
+    //! более правильно будет назвать переменную initialValue
+    const firstValue = initialValue.textContent;
+    const typeInput = initialValue.dataset.type;
+    initialValue.classList.add("editing");
+    initialValue.innerHTML = `<input type=${typeInput} min="18" max="65">`;
 
-    const inputTD = clickedTd.querySelector("input"); // объявляю здеть, т.к. до этого момента input не существует
+    const inputTD = initialValue.querySelector("input"); // объявляю здеть, т.к. до этого момента input не существует
 
     inputTD.value = firstValue;
     inputTD.focus();
 
     inputTD.addEventListener("blur", makeChanges);
-    inputTD.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        makeChanges();
-      }
-    });
+    inputTD.addEventListener("keydown", (e) =>
+      e.key === "Enter" ? makeChanges() : false
+    );
 
     //не придумал как сделать без вложенной функции, многовато аргументов нужно передавать при вызове в моем варианте
+    //! можно оставить и так, либо передать в функцию объект со всеми нужными аргументами
     function makeChanges() {
       const secondValue = inputTD.value;
       const validateRusult = validateInput(typeInput, secondValue);
       if (validateRusult) {
-        clickedTd.textContent = firstValue;
-        clickedTd.classList.add("error-tooltip");
+        initialValue.textContent = firstValue;
+        initialValue.classList.add("error-tooltip");
         setTimeout(() => {
-          clickedTd.classList.remove("error-tooltip");
+          initialValue.classList.remove("error-tooltip");
         }, 2000);
       } else {
-        clickedTd.textContent = secondValue;
-        clickedTd.classList.remove("editing");
-        document.addEventListener("click", whereClick);
+        initialValue.textContent = secondValue;
+        //! если  здесь удаляешь класс, то вместо inputTD.focus() нужно было делать classList.add
+        // initialValue.classList.remove("editing");
+
+        //! здесь не нужно повторно добавлять обработчик событий — document будет отслеживать клики до тех пор, пока не будет использован removeEventListener
+        // document.addEventListener("click", whereClick);
       }
-      clickedTd.classList.remove("editing");
+      //! поскольку это действие необходимо выполнять в обоих случаях, его можно вынести за пределы ветви else и таким образом убрать дублирование
+      initialValue.classList.remove("editing");
     }
   }
 
@@ -77,5 +81,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  document.addEventListener("click", whereClick);
+  //! лучше сделать следующим образом, названить обработчик события на таблицы и на кнопку btnAddRow
+  // document.addEventListener("click", whereClick);
+  tableBody.addEventListener("click", chooseCellForChanges);
+  btnAddRow.addEventListener("click", addNewRow);
 });
